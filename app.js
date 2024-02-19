d57e420df7bb8bb78ba1c24f5f4dcb40bdd6572b
Maurilio Saddoud <masaddoud@davidson.edu>
2024-02-19 00:01:44 -0500

    removed weather api code

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const cors = require('cors')
 const openaiRoutes = require("./routes/openaiRoutes")
-const weatherRoutes = require("./routes/weatherRoutes")
 
 const app = express();
 app.use(express.json());
 app.use(cors())
 
 app.use("/api/openai", openaiRoutes);
-app.use('/api/weather', weatherRoutes); 
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
